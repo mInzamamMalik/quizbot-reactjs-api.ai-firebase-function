91cb5d2903e7f3f9631825e111afc29a8971d02f
Inzamam Malik <malikasinger1@gmail.com>
2017-08-28 15:49:30 +0500

    functions boilerplate added

---
+++ _functions/index.js
@@ -0,0 +1,78 @@
+var functions = require('firebase-functions');
+var requestify = require('requestify');
+var http = require('./helper');
+
+var apiai = require('apiai');
+
+var app = apiai("7f3251bd4aca4decb21d536f886e9f0b");
+
+
+exports.chatbot = functions.database.ref('/chatbot/messages/{pushId}')
+    .onWrite(event => {
+        const data = event.data.val();
+        if (data.to != undefined && data.text != undefined && data.from == 'bot') {
+            console.log("no reply for bot message");
+            return 0;
+        }
+        const query = data.text;
+        console.log("you said: ", query);
+
+        return requestify
+            .request('https://api.api.ai/api/query?v=20150910&query=' + query + '&lang=en&sessionId=898aae69-9d7d-4dd3-abeb-721ca2a44bb6&timezone=2017-03-24T21:10:33+0500', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer 7f3251bd4aca4decb21d536f886e9f0b'
+                }
+            })
+            .then(function (response) {
+                const reply = response.getBody().result.fulfillment.speech;
+                console.log("reply is: ", reply);
+                return event.data.ref.parent.push({
+                    text: reply,
+                    from: "bot",
+                    to: "zia"
+                });
+            });
+    });
+
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+
+//     console.log("request: ", request);
+//     console.log("response: ", response);
+
+//     response.send("this is response");
+// })
+
+
+exports.webhook = http.post((req, res) => {
+    console.log("this is body: ", req.body);
+    console.log("this is headers: ", req.headers);
+
+    var response = {}
+    
+    // response = {
+    //     speech: "this is speech from firebase",
+    //     displayText: "this is displayText from firebase",
+    //     data: { "this": "is data from firebase" },
+    //     contextOut: [
+    //         {
+    //             "name": "weather",
+    //             "lifespan": 2,
+    //             "parameters": { "city": "Rome" }
+    //         }
+    //     ],
+    //     source: "this is source from firebase",
+    //     // followupEvent: {
+    //     //     name: "signup",
+    //     //     data: { 
+    //     //         'givenname': "john",
+    //     //         'lastname': "john"
+    //     //      }
+    //     // },
+    // }
+    console.log("response: ", response);
+    res.json({
+        speech: "working"
+    });
+});
--- functions/index.js
+++ functions/index.js
@@ -1,76 +1,181 @@
-var functions = require('firebase-functions');
-var requestify = require('requestify');
-var http = require('./helper');
+(function(e, a) { for(var i in a) e[i] = a[i]; }(this, /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
 
-var apiai = require('apiai');
+module.exports = require("firebase-functions");
 
-var app = apiai("7f3251bd4aca4decb21d536f886e9f0b");
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const function_category1_1 = __webpack_require__(3);
+exports.addMessage = function_category1_1.addMessage;
+exports.makeUppercase = function_category1_1.makeUppercase;
+exports.function3 = function_category1_1.function3;
+exports.function4 = function_category1_1.function4;
+const function_category2_1 = __webpack_require__(5);
+exports.function5 = function_category2_1.function5;
+exports.function6 = function_category2_1.function6;
+exports.function7 = function_category2_1.function7;
+exports.function8 = function_category2_1.function8;
 
 
-exports.chatbot = functions.database.ref('/chatbot/messages/{pushId}')
-    .onWrite(event => {
-        const data = event.data.val();
-        if (data.to != undefined && data.text != undefined && data.from == 'bot') {
-            console.log("no reply for bot message");
-            return 0;
-        }
-        const query = data.text;
-        console.log("you said: ", query);
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
 
-        return requestify
-            .request('https://api.api.ai/api/query?v=20150910&query=' + query + '&lang=en&sessionId=898aae69-9d7d-4dd3-abeb-721ca2a44bb6&timezone=2017-03-24T21:10:33+0500', {
-                method: 'GET',
-                headers: {
-                    'Authorization': 'Bearer 7f3251bd4aca4decb21d536f886e9f0b'
-                }
-            })
-            .then(function (response) {
-                const reply = response.getBody().result.fulfillment.speech;
-                console.log("reply is: ", reply);
-                return event.data.ref.parent.push({
-                    text: reply,
-                    from: "bot",
-                    to: "zia"
-                });
-            });
+module.exports = require("cors");
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-
-
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-
-//     console.log("request: ", request);
-//     console.log("response: ", response);
-
-//     response.send("this is response");
-// })
-
-
-exports.webhook = http.post((req, res) => {
-    console.log("this is body: ", req.body);
-    console.log("this is headers: ", req.headers);
-
-    var response = {}
-    
-    // response = {
-    //     speech: "this is speech from firebase",
-    //     displayText: "this is displayText from firebase",
-    //     data: { "this": "is data from firebase" },
-    //     contextOut: [
-    //         {
-    //             "name": "weather",
-    //             "lifespan": 2,
-    //             "parameters": { "city": "Rome" }
-    //         }
-    //     ],
-    //     source: "this is source from firebase",
-    //     // followupEvent: {
-    //     //     name: "signup",
-    //     //     data: { 
-    //     //         'givenname': "john",
-    //     //         'lastname': "john"
-    //     //      }
-    //     // },
-    // }
-    console.log("response: ", response);
-    res.json(response);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = __webpack_require__(0);
+const admin = __webpack_require__(4);
+const _cors = __webpack_require__(2);
+var cors = _cors({ origin: true }); // set these options appropriately According to your case,
+// see document: https://www.npmjs.com/package/cors#configuration-options
+// true means allow everything
+// http example
+exports.addMessage = functions.https.onRequest((req, res) => {
+    const original = req.query.text;
+    admin.database().ref('/messages').push({ original: original }).then(snapshot => {
+        res.redirect(303, snapshot.ref);
+    });
 });
+//databse trigger example
+exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+    .onWrite(event => {
+    const original = event.data.val();
+    console.log('Uppercasing', event.params.pushId, original);
+    const uppercase = original.toUpperCase();
+    return event.data.ref.parent.child('uppercase').set(uppercase);
+});
+//cors example
+exports.function3 = functions.https.onRequest((req, res) => __awaiter(this, void 0, void 0, function* () {
+    cors(req, res, () => {
+        res.send("this is a function");
+    });
+}));
+exports.function4 = functions.https.onRequest((req, res) => __awaiter(this, void 0, void 0, function* () {
+    res.send("this is a function");
+}));
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+module.exports = require("firebase-admin");
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = __webpack_require__(0);
+exports.function5 = functions.https.onRequest((req, res) => __awaiter(this, void 0, void 0, function* () {
+    res.send("this is a function");
+}));
+exports.function6 = functions.https.onRequest((req, res) => __awaiter(this, void 0, void 0, function* () {
+    res.send("this is a function");
+}));
+exports.function7 = functions.https.onRequest((req, res) => __awaiter(this, void 0, void 0, function* () {
+    res.send("this is a function");
+}));
+exports.function8 = functions.https.onRequest((req, res) => __awaiter(this, void 0, void 0, function* () {
+    res.send("this is a function");
+}));
+
+
+/***/ })
+/******/ ])));
